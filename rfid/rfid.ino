90605751ab2c166762374e5ef6c68361139ac7aa
Wassim Sellami <wassim.sellami@supcom.tn>
2023-01-19 16:39:57 +0100

    525

--- rfid/rfid.ino
+++ rfid/rfid.ino
@@ -4,47 +4,89 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecureBearSSL.h>
 #include <ArduinoJson.h>
+#include "Adafruit_Keypad.h"
+
+unsigned long wakeUpPeriod = 100000;
+
+//KEY PAD VARIABLES
+const byte ROWS = 4; // rows
+const byte COLS = 4; // columns
+//define the symbols on the buttons of the keypads
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+byte rowPins[ROWS] = {16, 5, 4, 0}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {2, 14, 12, 13}; //connect to the column pinouts of the keypad
+
+Adafruit_Keypad customKeypad = Adafruit_Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 
-//const uint8_t fingerprint[20] = {0x40, 0xaf, 0x00, 0x6b, 0xec, 0x90, 0x22, 0x41, 0x8e, 0xa3, 0xad, 0xfa, 0x1a, 0xe8, 0x25, 0x41, 0x1d, 0x1a, 0x54, 0xb3};
+//WIFI VARIABLES
 const char* ssid = "SiAymen";
 const char* pwd = "12345678";
 const String url = "https://node-js-test-9u9r.onrender.com/";
-
+const String codes[4] = {"1234", "5678", "0000"};
 ESP8266WiFiMulti WiFiMulti;
+
+unsigned long lastms=0;
 
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFiMulti.addAP(ssid, pwd);
+  customKeypad.begin();
 }
 
 void loop() {
+  char c = 0;
+  customKeypad.tick();
+  while (customKeypad.available()) {
+    keypadEvent e = customKeypad.read();
+    if (e.bit.EVENT == KEY_JUST_PRESSED){
+      c = (char)e.bit.KEY;
+    }
+  }
   if ((WiFiMulti.run() == WL_CONNECTED)) {
-
     std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
     client->setInsecure();
     HTTPClient https;
-    String name="";
-    boolean isPresent = false;
-
-    String memberCode = "1234";
-    
-    if(getMemberByCode(client, https, "get-present", memberCode, name, isPresent )){
-      Serial.println(name);
+    if(c){
+      int codeIndex = c-'0'-1;
+      String memberCode = codes[codeIndex];
+      Serial.println(memberCode);
       
-      
-    }else{
-      Serial.print("[HTTPS] Unable to connect\n");
-
+      String name = "";
+      boolean isPresent = false;
+      if (getMemberByCode(client, https, "get-member", memberCode, name, isPresent )) {
+        if (name != "") {
+          Serial.print("name : ");
+          Serial.println(name);
+          Serial.print("is Present: ");
+          Serial.println(isPresent);
+          setPresence(client, https, "set-presence", memberCode, !isPresent);
+        } else {
+          Serial.println("Member not found !");
+        }
+      } else {
+        Serial.println("[HTTPS] Unable to connect\n");
+      }
+      delay(2000);
+      Serial.println("waiting for other code");
     }
-    Serial.println("Wait 3s before next round...");
-    delay(3000);
+
+
+    // periodic wake up of the server.
+    if((millis()-lastms)>=wakeUpPeriod)
+    {
+      lastms=millis();
+      Serial.println("waking up the server !");
+      periodicGet(client, https, "get-present");
+    }
   }
-
-  
-
-  
+  delay(10);
 }
 
 
@@ -52,26 +94,72 @@ void loop() {
 
 
 
-boolean getMemberByCode(std::unique_ptr<BearSSL::WiFiClientSecure>& client, HTTPClient& https,
-                          String route, String code, String& name, boolean& isPresent){
-  if (https.begin(*client, url+route)) {  
+boolean getMemberByCode(std::unique_ptr<BearSSL::WiFiClientSecure>& client, HTTPClient & https,
+                        String route, String code, String & name, boolean & isPresent) {
+  if (https.begin(*client, url + route)) {
+    https.addHeader("Content-Type", "application/json");
+    String requestBody = "{\"code\":\"" + code + "\"}";
+    int httpCode = https.POST(requestBody);
+    if (httpCode > 0) {
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+        String response = https.getString();
+        StaticJsonDocument<200> responseBody;
+        DeserializationError error = deserializeJson(responseBody, response);
+        if (responseBody["success"]) {
+          name = (String)responseBody["data"]["name"];
+          isPresent = (boolean)responseBody["data"]["is_present"];
+        } else {
+          name = "";
+        }
+      }
+    } else {
+      Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+    }
+
+    https.end();
+    return true;
+  }
+  return false;
+}
+
+boolean setPresence(std::unique_ptr<BearSSL::WiFiClientSecure>& client, HTTPClient & https,
+                    String route, String code, boolean newPresence) {
+  if (https.begin(*client, url + route)) {
+    https.addHeader("Content-Type", "application/json");
+    String requestBody = "{\"code\":\"" + code + "\", \"is_present\":" + newPresence + "}";
+    int httpCode = https.POST(requestBody);
+    if (httpCode > 0) {
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+        String response = https.getString();
+        StaticJsonDocument<200> responseBody;
+        DeserializationError error = deserializeJson(responseBody, response);
+        Serial.println((String)responseBody["message"]);
+      }
+    } else {
+      Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+    }
+
+    https.end();
+    return true;
+  }
+  return false;
+}
+
+void periodicGet(std::unique_ptr<BearSSL::WiFiClientSecure>& client, HTTPClient & https,
+                    String route) {
+    if(https.begin(*client, url + route)){
       int httpCode = https.GET();
       if (httpCode > 0) {
         if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          String payload = https.getString();
-          StaticJsonDocument<200> doc;
-          DeserializationError error = deserializeJson(doc, payload);
-          name = (String)doc["data"]["name"];
-          isPresent = (boolean)doc["data"]["is_present"];
-
-             
+          String response = https.getString();
+          StaticJsonDocument<200> responseBody;
+          DeserializationError error = deserializeJson(responseBody, response);
+          Serial.println((String)responseBody["message"]);
         }
       } else {
         Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
       }
-
+  
       https.end();
-      return true;
-    } 
-    return false;
+    }
 }
